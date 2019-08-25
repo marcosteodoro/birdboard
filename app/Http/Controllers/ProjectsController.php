7542d887c21851c69cd7204c465e590ef8811d9a
Marcos Teodoro <marcosvinici_us@live.com>
2019-08-25 15:44:15 -0300

    Implementa teste unitario dos projetos e o metodo show

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -16,9 +16,19 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+        $attributes = request()->validate([
+            'title' => 'required', 
+            'description' => 'required'
+        ]);
 
         Project::create($attributes);
         return redirect('/projects');
+    }
+
+    public function show(Project $project)
+    {
+        return view('projects.show', [
+            'project' => $project
+        ]);
     }
 }
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,8 +9,14 @@
 <body>
     <h1>Birdboard</h1>
 
-    @foreach ($projects as $project)
-        <li>{{ $project->title }}</li>
-    @endforeach
+    @forelse ($projects as $project)
+        <li>
+            <a href="{{ $project->path() }}">
+                {{ $project->title }}                
+            </a>
+        </li>
+    @empty
+        <li>No projects yet.</li>
+    @endforelse
 </body>
 </html>
