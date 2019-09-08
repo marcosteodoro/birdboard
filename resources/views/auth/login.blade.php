a0bfcaafcdb7810c4cd29da07fa00c3d981a60ab
Marcos Teodoro <marcosvinici_us@live.com>
2019-09-08 10:33:54 -0300

    Ajusta layout para utilizar componentização do estilo dos elementos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="form-area bg-white rounded-lg p-8 lg:px-20 lg:w-2/3 mx-auto shadow-md">
-        <h1 class="font-normal text-center is-1 mb-8">Login</h1>
+    <div class="form-area">
+        <h1 class="heading">Login</h1>
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <div>
-                <label class="block mb-2" for="email">{{ __('E-Mail Address') }}</label>
-                <div class="mb-8">
-                    <input id="email" type="email"
-                           class="input appearance-none border rounded w-full py-2 px-3 @error('email') text-red @enderror"
+                <label class="label" for="email">{{ __('E-Mail Address') }}</label>
+                <div class="field">
+                    <input id="email" type="email" class="input @error('email') text-red @enderror"
                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                     @error('email')
                     <div class="mt-1">
-                        <span class="text-red text-xs italic mt-24">{{ $message }}</span>
+                        <span class="input-error">{{ $message }}</span>
                     </div>
                     @enderror
                 </div>
             </div>
 
-            <div class="mb-8">
+            <div class="field">
                 <label for="password"
-                       class="block mb-2">{{ __('Password') }}</label>
+                       class="label">{{ __('Password') }}</label>
                 <div>
-                    <input id="password" type="password"
-                           class="input appearance-none border rounded w-full py-2 px-3 @error('password') text-red @enderror"
+                    <input id="password" type="password" class="input @error('password') text-red @enderror"
                            name="password" required autocomplete="current-password">
                     @error('password')
                     <div class="mt-1">
-                        <span class="text-red text-xs italic mt-24">{{ $message }}</span>
+                        <span class="input-error">{{ $message }}</span>
                     </div>
                     @enderror
                 </div>
             </div>
 
-            <div class="mb-8">
-                <input class="mr-2 leading-tight" type="checkbox" name="remember"
+            <div class="field">
+                <input class="input checkbox" type="checkbox" name="remember"
                        id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                 <label for="remember">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="form-area bg-white rounded-lg p-8 lg:px-20 lg:w-2/3 mx-auto shadow-md">
-        <h1 class="font-normal text-center is-1 mb-8">Register</h1>
+    <div class="form-area">
+        <h1 class="heading">Register</h1>
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <div>
+           <div>
                 <label for="name" class="block mb-2">{{ __('Name') }}</label>
 
-                <div class="mb-8">
-                    <input id="name" type="text"
-                           class="input appearance-none border rounded w-full py-2 px-3 @error('name') is-invalid @enderror" name="name"
+                <div class="field">
+                    <input id="name" type="text" class="input @error('name') is-invalid @enderror" name="name"
                            value="{{ old('name') }}" required autocomplete="name" autofocus>
                     @error('name')
                     <div class="mt-1">
-                        <span class="text-red text-xs italic mt-24">{{ $message }}</span>
+                        <span class="input-error">{{ $message }}</span>
                     </div>
                     @enderror
                 </div>
@@ -25,14 +24,12 @@
             <div>
                 <label for="email" class="block mb-2">{{ __('E-Mail Address') }}</label>
 
-                <div class="mb-8">
-                    <input id="email" type="email"
-                           class="input appearance-none border rounded w-full py-2 px-3 @error('email') is-invalid @enderror"
+                <div class="field">
+                    <input id="email" type="email" class="input @error('email') is-invalid @enderror"
                            name="email" value="{{ old('email') }}" required autocomplete="email">
-
                     @error('email')
                     <div class="mt-1">
-                        <span class="text-red text-xs italic mt-24">{{ $message }}</span>
+                        <span class="input-error">{{ $message }}</span>
                     </div>
                     @enderror
                 </div>
@@ -41,13 +38,13 @@
             <div>
                 <label for="password" class="block mb-2">{{ __('Password') }}</label>
 
-                <div class="mb-8">
+                <div class="field">
                     <input id="password" type="password"
-                           class="input appearance-none border rounded w-full py-2 px-3 @error('password') is-invalid @enderror"
+                           class="input @error('password') is-invalid @enderror"
                            name="password" required autocomplete="new-password">
                     @error('password')
                     <div class="mt-1">
-                        <span class="text-red text-xs italic mt-24">{{ $message }}</span>
+                        <span class="input-error">{{ $message }}</span>
                     </div>
                     @enderror
                 </div>
@@ -56,9 +53,9 @@
             <div>
                 <label for="password-confirm" class="block mb-2">{{ __('Confirm Password') }}</label>
 
-                <div class="mb-8">
+                <div class="field">
                     <input id="password-confirm" type="password"
-                           class="input appearance-none border rounded w-full py-2 px-3" name="password_confirmation"
+                           class="input" name="password_confirmation"
                            required autocomplete="new-password">
                 </div>
             </div>
