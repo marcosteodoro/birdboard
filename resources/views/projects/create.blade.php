196f4f82227ff825e551c6abef267729fdefdcd0
Marcos Teodoro <marcosvinici_us@live.com>
2019-09-08 10:34:16 -0300

    Aplica layout no formulário de criação do projeto

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,30 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="/projects" method="POST">
-    @csrf
-    <h1 class="heading is-1">Create a Project</h1>
-    <div class="field">
-        <label for="title">Title</label>
-        <div class="control">
-            <input type="text" class="input" name="title" placeholder="Title">
-        </div>
+    <div class="form-area">
+        <form action="/projects" method="POST">
+            @csrf
+            <h1 class="heading is-1">Create a Project</h1>
+            <div class="field">
+                <label class="label" for="title">Title</label>
+                <input type="text" class="input" name="title" placeholder="Title">
+            </div>
+
+            <div class="field">
+                <label class="label" for="description">Description</label>
+                <textarea name="description" class="input textarea"></textarea>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <button type="submit" class="button is-link">Create Project</button>
+                    <a href="/projects">Cancel</a>
+                </div>
+            </div>
+
+        </form>
     </div>
-
-    <div class="field">
-        <label for="description">Description</label>
-
-        <div class="control">
-            <textarea name="description" class="textarea"></textarea>
-        </div>
-    </div>
-
-    <div class="field">
-        <div class="control">
-            <button type="submit" class="button is-link">Create Project</button>
-            <a href="/projects">Cancel</a>
-        </div>
-    </div>
-
-</form>
 @endsection
+ 
