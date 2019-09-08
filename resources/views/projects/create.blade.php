c49c276305f118e895ec9961ce1ed339bd2a20e8
Marcos Teodoro <marcosvinici_us@live.com>
2019-09-08 10:47:06 -0300

    Aplica ajustes de erro no campo de descrição do projeto

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -7,12 +7,22 @@
             <h1 class="heading is-1">Create a Project</h1>
             <div class="field">
                 <label class="label" for="title">Title</label>
-                <input type="text" class="input" name="title" placeholder="Title">
+                <input type="text" class="input @error('title') is-invalid @enderror" name="title" placeholder="Title" value="{{ old('title') }}">
+                @error('title')
+                <div class="mt-1">
+                    <span class="input-error">{{ $message }}</span>
+                </div>
+                @enderror
             </div>
 
             <div class="field">
                 <label class="label" for="description">Description</label>
-                <textarea name="description" class="input textarea"></textarea>
+                <textarea name="description" class="input textarea @error('description') is-invalid @enderror">{{ old('description') }}</textarea>
+                @error('description')
+                <div class="mt-1">
+                    <span class="input-error">{{ $message }}</span>
+                </div>
+                @enderror
             </div>
 
             <div class="field">
@@ -25,4 +35,4 @@
         </form>
     </div>
 @endsection
- 
+
