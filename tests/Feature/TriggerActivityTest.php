f1b557c73fc56bee876d3643c93c94619460e0d8
Marcos Teodoro <marcosvinici_us@live.com>
2019-09-21 18:28:31 -0300

    Detalha atualização das tasks nos detalhes do projeto
    Usa relacionamento polimórfico para relacionar tasks e activities, dessa forma temos acesso à task que gerou determinada activity

--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Task;
 use Facades\Tests\Setup\ProjectFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -37,7 +38,12 @@ class TriggerActivityTest extends TestCase
 
         $project->addTask('Some task');
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('created_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('created_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+            $this->assertEquals('Some task', $activity->subject->body);
+        });
     }
 
     public function test_completing_a_task()
@@ -51,7 +57,11 @@ class TriggerActivityTest extends TestCase
             ]);
 
         $this->assertCount(3, $project->activity);
-        $this->assertEquals('completed_task', $project->activity->last()->description);
+
+        tap($project->activity->last(), function ($activity) {
+            $this->assertEquals('completed_task', $activity->description);
+            $this->assertInstanceOf(Task::class, $activity->subject);
+        });
     }
 
     public function test_incompleting_a_task()
